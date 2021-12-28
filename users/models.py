17a30a1b6efeaa6c41d2baa2ca14125d2e989a62
Oleg <didoleg@mail.ru>
2021-12-28 13:50:36 +0700

    lesson_1. Add Users

--- users/models.py
+++ users/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from uuid import uuid4
 
 
-class Author(models.Model):
+class User(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+    user_name = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    birthday_year = models.PositiveIntegerField()
+    email = models.CharField(default=uuid4, unique=True, max_length=100)
 
