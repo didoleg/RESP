72626b1a0416967daf4ead1afb66937fcfa0c49b
Oleg <didoleg@mail.ru>
2022-01-25 14:40:35 +0700

    lesson_5: App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,95 @@
-import React from 'react'
-// import AuthorList from './components/Author.js'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import axios from 'axios'
+import './bootstrap/css/bootstrap.min.css'
+import './bootstrap/css/sticky-footer-navbar.css'
+import Footer from './components/Footer.js'
+import Navbar from './components/Menu.js'
 import UserList from './components/User.js'
+import {ProjectList, ProjectDetail} from './components/Project.js'
+import ToDoList from './components/ToDo.js'
+
+
+
+const DOMAIN = 'http://localhost:8000/viewsets'
+const get_url = (url) => `${DOMAIN}${url}`
+
 
 class App extends React.Component {
-
     constructor(props) {
         super(props)
         this.state = {
-            'users': [],
+            navbarItems: [
+                {name: 'Users', href: '/users/'},
+                {name: 'Projects', href: '/Project/'},
+                {name: 'ToDo', href: '/ToDo/'},
+            ],
+            users: [],
+            projects: [],
+            project: {},
+            todos: []
         }
     }
 
-    componentDidMount() {
-        axios.get('http://localhost:8000/viewsets/users/')
-            .then(response => {
-                const users = response.data;
-                this.setState({
-                    'users': users,
-                });
-            })
-            .catch(error => console.log(error));
+    render() {
+         return (
+              <Router>
+                  <header>
+                    <Navbar navbarItems={this.state.navbarItems} />
+                  </header>
+                  <main role="main" class="flex-shrink-0">
+                      <div className="container">
+                        <Switch>
+                            <Route exact path='/'>
+                                <UserList users={this.state.users} />
+                            </Route>
+                            <Route exact path='/projects'>
+                                <ProjectList items={this.state.projects} />
+                            </Route>
+                            <Route exact path='/todos'>
+                                <ToDoList items={this.state.todos} />
+                            </Route>
+                            <Route path="/project/:id" children={<ProjectDetail getProject={(id) => this.getProject(id)} item={this.state.project} />} />
+                        </Switch>
+                      </div>
+                  </main>
+
+              <Footer />
+            </Router>
+
+
+            )
     }
 
-    render() {
-        return (
-        <div className="App">
-            <UserList items={this.state.users} />
-        </div>
-        )
+    getProject(id) {
+        console.log('call')
+        console.log(get_url(`/api/projects/${id}`))
+        axios.get(get_url(`/api/projects/${id}`))
+        .then(response => {
+            console.log(response.data)
+            this.setState({project: response.data})
+        }).catch(error => console.log(error))
+    }
+
+
+    componentDidMount() {
+            axios.get(get_url('/users/'))
+        .then(response => {
+            this.setState({users: response.data.results})
+        }).catch(error => console.log(error))
+
+        axios.get(get_url('/Project/'))
+        .then(response => {
+            this.setState({projects: response.data.results})
+        }).catch(error => console.log(error))
+
+        axios.get(get_url('/ToDo/'))
+        .then(response => {
+            this.setState({todos: response.data.results})
+        }).catch(error => console.log(error))
+
     }
 }
+
 
 export default App;
