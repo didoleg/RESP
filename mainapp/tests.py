ceba6e2bcea166a0cd9e8420bdcc51c3f968e5ff
Oleg <didoleg@mail.ru>
2022-02-03 17:08:50 +0700

    lesson_8: add APIRequestFactory test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,16 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from users.views import UserViewSet
+from users.models import User
 
-# Create your tests here.
+
+def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/Users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
