4d9db70881935f4ef7a224f662080c7071af3dbf
Oleg <didoleg@mail.ru>
2022-02-09 11:59:21 +0700

    lesson_9: add new_models

--- library/settings.py
+++ library/settings.py
@@ -149,6 +149,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
    'PAGE_SIZE': 100
 }
--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from uuid import uuid4
 
@@ -7,5 +8,6 @@ class User(models.Model):
     user_name = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     email = models.CharField(default=uuid4, unique=True, max_length=100)
-
