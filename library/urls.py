fd1a61aac3955a975c9f5437b7c4030c782cc10a
Oleg <78960452+didoleg@users.noreply.github.com>
2022-02-14 02:22:01 +0700

    Lesson 9 (#10)
    * lesson_9: add new model users
    
    * lesson_9: add users version
    
    * lesson_9: yasg.py
    
    * lesson_9: add doc

--- library/urls.py
+++ library/urls.py
@@ -18,6 +18,23 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from mainapp.views import UserMyViewSet, ProjectMyViewSet, ToDOViewSet, FilterProject, FilterToDO
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library",
+      default_version='1.0',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="admin@admin.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 router = DefaultRouter()
 router.register('Users', UserMyViewSet)
@@ -32,4 +49,9 @@ urlpatterns = [
    path('filter/projects/<str:name>/', FilterProject.as_view()),
    path('filter/projects/<str:name>/', FilterToDO.as_view()),
    path('api-token-auth/', views.obtain_auth_token),
+   path('api/users/1', include('users.urls', namespace='1')),
+   path('api/users/2', include('users.urls', namespace='2')),
+   path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
